bea80e67c0bc8e706f760ab9c76f7c12c923d42f
Ie Xiann Goh <ixgoh@outlook.com>
2023-02-19 12:40:53 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,9 +4,9 @@ read -p 'Target Hostname: ' hostname
 read -p 'Target Username: ' username
 read -sp 'Target Password: ' password
 echo
-sed -i "s/hostname/$hostname/g" inventory2
-sed -i "s/my_user/$username/g" inventory2
-sed -i "s/my_password/$password/g" inventory2
+sed -i "s/hostname/$hostname/g" inventory
+sed -i "s/my_user/$username/g" inventory
+sed -i "s/my_password/$password/g" inventory
 sudo apt update && sudo apt install -y python3-pip sshpass
 pip3 install ansible
 $HOME/.local/bin/ansible-galaxy install -r requirements.yml
