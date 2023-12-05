bddd47932b7af54298872863c10c20ed9e6846fa
Ie Xiann Goh <ixgoh@outlook.com>
2023-12-05 11:23:59 +0800

    Add CPU Governor & Code refactor (#4)
    * enhancement: sed string replacement
    
    * Remvove trailing whitespace & Rename task
    
    * Fix risky octet
    
    * Disable netplan
    
    * Add configure CPU governor

--- run.sh
+++ run.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+set -euo pipefail
+
 read -p 'Target Hostname: ' hostname
 read -p 'Target Username: ' username
 read -sp 'Target Password: ' password
 echo
-sed -i "s/hostname/$hostname/g" inventory
-sed -i "s/my_user/$username/g" inventory
-sed -i "s/my_password/$password/g" inventory
+
+sed -i -e "s/^\(.*\) ansible_user/$hostname ansible_user/" \
+       -e "s/ansible_user=[^[:space:]]*/ansible_user=$username/" \
+       -e "s/ansible_password=[^[:space:]]*/ansible_password=$password/" \
+       -e "s/ansible_become_password=[^[:space:]]*/ansible_become_password=$password/" inventory
+
 sudo apt update && sudo apt install -y python3-pip sshpass
 pip3 install ansible
 $HOME/.local/bin/ansible-galaxy install -r requirements.yml
